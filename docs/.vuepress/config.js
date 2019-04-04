3189f045a8d042debe4cac4f809e7a68917c5104
Thomas Betous <tbetous@gmail.com>
2019-04-04 17:05:15 +0200

    :sparkles: improve favicon
    improve favicon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,46 @@
 module.exports = {
   title: 'bento-starter',
   description: 'Prepare your PWA faster than your launch !',
-  head: [['link', { rel: 'icon', href: '/assets/img/icons/logo.png' }]],
+  head: [
+    ['link', { rel: 'shortcut icon', href: '/assets/img/icons/favicon.ico' }],
+    [
+      'link',
+      {
+        rel: 'icon',
+        href: '/assets/img/icons/favicon-16x16.png',
+        size: '16x16'
+      }
+    ],
+    [
+      'link',
+      {
+        rel: 'icon',
+        href: '/assets/img/icons/favicon-32x32.png',
+        size: '32x32'
+      }
+    ],
+    [
+      'link',
+      {
+        rel: 'apple-touch-icon',
+        href: '/assets/img/icons/apple-touch-icon.png',
+        size: '180x180'
+      }
+    ],
+    [
+      'link',
+      {
+        rel: 'mask-icon',
+        href: '/assets/img/icons/safari-pinned-tab.svg',
+        color: '#5bbad5'
+      }
+    ],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#603cba' }],
+    ['meta', { name: 'msapplication-config', content: '/browserconfig.xml' }],
+    ['meta', { name: 'theme-color', content: '#ffffff' }]
+  ],
+  serviceWorker: true,
   themeConfig: {
     logo: '/assets/img/bento-starter.svg',
     nav: [
